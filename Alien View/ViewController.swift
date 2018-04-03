fb86060fd4cc95117e2a234c6de071c353985322
D7703_03 <d7703_03@D7703-03ui-iMac.local>
2018-04-03 15:34:52 +0900

    add alien image view

--- Alien View/ViewController.swift
+++ Alien View/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 1
+    var chk = true
+    
+    @IBOutlet weak var alienImageView: UIImageView!
+    @IBOutlet weak var lblCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        alienImageView.image = UIImage(named: "frame1.png")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func updateImage(_ sender: Any) {
+        if counter == 5 {
+            chk = false
+           counter = counter - 1
+        } else if counter == 1 {
+            chk = true
+            counter = counter + 1
+        } else if chk == false {
+            counter = counter - 1
+        } else if chk == true {
+            counter = counter + 1
+        }
+        alienImageView.image = UIImage(named: "frame\(counter).png")
+        lblCount.text = "\(counter)"
+    }
 }
 
